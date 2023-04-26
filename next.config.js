59a4f69ce552108299ec24f6bf5ac0937167e873
AdolfUrian <adolfjamesurian@gmail.com>
2023-04-26 21:05:16 +0800

    runtime edge

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     images: {
         domains: ["avatars.githubusercontent.com"],
     },
+    experimental: {
+        runtime: "edge",
+    },
 };
 
 module.exports = nextConfig;
