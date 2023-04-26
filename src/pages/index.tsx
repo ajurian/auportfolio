b4447cdcb5b4930dda23ce7deaac8c8786d3cae1
AdolfUrian <adolfjamesurian@gmail.com>
2023-04-26 20:44:02 +0800

    Keep serversideprops

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,19 +37,19 @@ const Home: NextPage<HomeProps> = ({ authUser, authPass, apiKey }) => {
     );
 };
 
-/* export const getServerSideProps: GetServerSideProps = async (ctx) => {
-    console.log("[code]: Server side props called...");
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    /* console.log("[code]: Server side props called...");
     console.log(
         `[code]: ${process.env.AUTH_USER} | ${process.env.AUTH_PASS} | ${process.env.API_KEY}`
-    );
+    ); */
 
     return {
         props: {
-            authUser: process.env.AUTH_USER,
+            /* authUser: process.env.AUTH_USER,
             authPass: process.env.AUTH_PASS,
-            apiKey: process.env.API_KEY,
+            apiKey: process.env.API_KEY, */
         },
     };
-}; */
+};
 
 export default Home;
